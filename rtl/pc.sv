0e11854869333d8ad4df4c4bb8c07dd6c2910557
Abdullah Chaudhary <abdullah.chaudhary1605@gmail.com>
2026-01-27 19:59:35 +0500

    add PC module

---
+++ rtl/pc.sv
@@ -0,0 +1,18 @@
+// 64-bit Program Counter 
+module pc (
+    input  logic        clk,
+    input  logic        rst,
+    input  logic [63:0] PCNext, // Address calculated by Fetch Stage logic
+    output logic [63:0] PC      // Current PC value for the Fetch stage
+);
+
+    always_ff @(posedge clk or posedge rst) begin
+        if (rst) begin
+            // common RISC-V reset vector is 0x0 or 0x80000000
+            PC <= 64'b0; 
+        end else begin
+            PC <= PCNext;
+        end
+    end
+
+endmodule
